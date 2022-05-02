f6d741cb558b127a8d20173f7c69a4bef1dbd076
Michelle Markl <michelle98.mm@gmail.com>
2022-05-02 15:24:42 +0200

    Add image analyzer
    Image analyzer gives us the possibility to edit each frame individually.

--- app/src/main/java/com/example/ml_kit/ThisIsMe.kt
+++ app/src/main/java/com/example/ml_kit/ThisIsMe.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -16,6 +18,8 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class ThisIsMe : Fragment() {
 
@@ -24,6 +28,13 @@ class ThisIsMe : Fragment() {
 
     private var REQUEST_CODE_PERMISSIONS = 123
     private var REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    // cameraX
+    private lateinit var preview: Preview
+    private lateinit var cameraSelector: CameraSelector
+    private lateinit var imageAnalysis: ImageAnalysis
+    private var executor: ExecutorService = Executors.newSingleThreadExecutor()
+
 
 
     override fun onCreateView(
@@ -99,31 +110,57 @@ class ThisIsMe : Fragment() {
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
         cameraProviderFuture.addListener({
+
             val cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
+            bindPreview()
+            imageAnalysis()
+
+            cameraProvider.bindToLifecycle(
+                context as LifecycleOwner, cameraSelector, imageAnalysis, preview
+            )
+
         }, ContextCompat.getMainExecutor(context))
     }
 
     /**
      * bindPreview
-     *      Input-val: cameraProvider: ProcessCameraProvider
      *
      * Set up camera, and execute camera activity.
      */
-    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
-        val preview = Preview.Builder()
+    private fun bindPreview() {
+        preview = Preview.Builder()
             .build()
 
-        var cameraSelector: CameraSelector = CameraSelector.Builder()
+        cameraSelector = CameraSelector.Builder()
             .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
             .build()
 
         var previewView: PreviewView = rootView.findViewById(R.id.me_preview)
 
         preview.setSurfaceProvider(previewView.surfaceProvider)
-        var camera = cameraProvider.bindToLifecycle(
-            this as LifecycleOwner, cameraSelector, preview
-        )
+    }
+
+    /**
+     * imageAnalysis
+     *
+     * TODO
+     */
+    private fun imageAnalysis() {
+        imageAnalysis = ImageAnalysis.Builder()
+            // enable the following line if RGBA output is needed.
+            // .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        imageAnalysis.setAnalyzer(executor, ImageAnalysis.Analyzer { imageProxy ->
+
+            // start face detection api
+            faceDetection(imageProxy)
+
+            // after done, release the ImageProxy object
+            imageProxy.close()
+        })
+
     }
 
 // ================================================================================================
@@ -131,4 +168,8 @@ class ThisIsMe : Fragment() {
     // ============================================================================================
     // Face Detection
     // ============================================================================================
+
+    private fun faceDetection(imageProxy: ImageProxy) {
+        // TODO: Face Detection
+    }
 }
