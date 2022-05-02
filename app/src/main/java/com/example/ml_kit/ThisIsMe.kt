fe7509b9726edb84e9fe3c1a3875610f53b57d8a
Michelle Markl <michelle98.mm@gmail.com>
2022-05-02 21:17:24 +0200

    Add outline Face

--- app/src/main/java/com/example/ml_kit/ThisIsMe.kt
+++ app/src/main/java/com/example/ml_kit/ThisIsMe.kt
@@ -4,10 +4,16 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -20,8 +26,10 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import org.w3c.dom.Text
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -141,6 +149,8 @@ class ThisIsMe : Fragment() {
 
         var previewView: PreviewView = rootView.findViewById(R.id.me_preview)
 
+        previewView.scaleType = PreviewView.ScaleType.FIT_CENTER
+
         preview.setSurfaceProvider(previewView.surfaceProvider)
     }
 
@@ -180,22 +190,59 @@ class ThisIsMe : Fragment() {
         val mediaImage = imageProxy.image
         if (mediaImage !== null) {
             val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            val commentbox: TextView = rootView.findViewById(R.id.me_commentbox)
 
             val detector = FaceDetection.getClient(realTimeOpts)
 
             detector.process(image)
                 .addOnSuccessListener { faces ->
                     for (face in faces) {
-                        // code
+                        outlineFace(face, imageProxy)
+                        commentbox.text = "yess"
                     }
                 }
                 .addOnFailureListener { _ ->
-                    // Task failed with an exception
-                    // ...
+
                 }
 
             // after done, release the ImageProxy object
             imageProxy.close()
         }
+    }
+
+    private fun outlineFace(face: Face, imageProxy: ImageProxy) {
+        val imageV: ImageView = rootView.findViewById(R.id.me_imageV)
+        val bitmap: Bitmap = Bitmap.createBitmap(imageV.width, imageV.height, Bitmap.Config.ARGB_8888)
+        val canvas: Canvas = Canvas(bitmap)
+
+        val paint = Paint()
+        paint.alpha = 0xA0                      // the transparency
+        paint.color = Color.RED                 // color is red
+        paint.style = Paint.Style.STROKE        // stroke or fill or ...
+        paint.strokeWidth = 10F                 // the stroke width
+
+        val preview_xcenter = imageV.width / 2F
+        val preview_ycenter = imageV.height / 2F
+
+        val face_xcenter = face.boundingBox.centerX()
+        val face_ycenter = face.boundingBox.centerY()
+
+        val xcenter = preview_xcenter - face_xcenter + face.boundingBox.width()
+        val ycenter = preview_ycenter - face_ycenter + face.boundingBox.height()
+
+        // Calculate positions.
+        val left = xcenter - face.boundingBox.width()
+        val top = ycenter + face.boundingBox.height()
+        val right = xcenter + face.boundingBox.width()
+        val bottom = ycenter - face.boundingBox.height()
+
+
+        val rec: Rect = Rect(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+
+        // draw Rect
+        canvas.drawRect(rec, paint)
+
+        // set bitmap as background to ImageView
+        imageV.background = BitmapDrawable(resources, bitmap)
     }
 }
